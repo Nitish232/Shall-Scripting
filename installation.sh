8b37837306c037f68c0d0c676e99089ff06cd8f3
Nitish232 <samanthakurthinitish@gmail.com>
2024-04-23 23:46:18 +0530

    Files

---
+++ installation.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+echo "User id $USERID"
+
+if [$USERID -ne 0]
+then 
+    echo "Please run this script with root access."
+    exit 1
+else 
+    echo "You are super user."
+fi
+
+dnf install mysql -y
+
+if [$? -ne 0]
+then 
+echo "Installation of mysql...Failure"
+else
+echo "Installation of mysql ...SUCCESS"
+fi
+
+dnf install git -y
+
+if [$? -ne 0]
+then 
+echo "Installation of git ...Failure"
+exit 1
+else
+echo "Installation of git....SUCCESSS"
+fi 
+
+echo "Is script proceeding?"
