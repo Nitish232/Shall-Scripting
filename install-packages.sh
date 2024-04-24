ad5540b956fa2cfeb538e5ee273abbccdcd1e7be
Nitish232 <samanthakurthinitish@gmail.com>
2024-04-24 21:24:33 +0530

    Files

--- install-packages.sh
+++ install-packages.sh
@@ -9,20 +9,21 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
+
 VALIDATE(){
-   if [ $1 -ne 0 ]
-   then
-        echo -e "$2...$R FAILURE $N"
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2...$R Failure $N"
         exit 1
     else
-        echo -e "$2...$G SUCCESS $N"
-    fi
+        echo -e "$2...$G Success $N"
+    fi 
 }
 
-if [ $USERID -ne 0 ]
-then
-    echo "Please run this script with root access."
-    exit 1 # manually exit if error comes.
+if [ $USERID -ne 0]
+then 
+    echo "please run this script with root access."
+    exit 1
 else
     echo "You are super user."
 fi
@@ -31,12 +32,11 @@ for i in $@
 do
     echo "package to install: $i"
     dnf list installed $i &>>$LOGFILE
-    if [ $? -eq 0 ]
-    then
-        echo -e "$i already installed...$Y SKIPPING $N"
+    if[ $? -eq 0 ]
+    then 
+        echo -e "$i already installed...$Y Skipping $N"
     else
         dnf install $i -y &>>$LOGFILE
         VALIDATE $? "Installation of $i"
     fi
 done
-
